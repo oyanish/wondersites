d53d38d4d332574345d8e8bb21d1fc474c9b9008
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-05-09 13:47:01 -0400

    edits footer template

--- footer.php
+++ footer.php
@@ -1,16 +1,19 @@
 
 		<footer class="main-footer">
 			<?php wp_footer(); ?>
-			<img src="img/inline-wondersites-logo.svg.php" alt="wondersites web design, development and marketing" class="footer-logo">
+			<div class="logo">
+				<?php get_template_part( 'img/inline', 'wondersites-logo.svg' ); ?>
+				<span class="slogan">Design | Development | Marketing</span>
+			</div>
 			<nav class="footer-menu">
 				<ul>
 					<li><a href="#">Services</a></li>
 					<li><a href="#">About us</a></li>
 					<li><a href="#">Our work</a></li>
 					<li><a href="#">Contact us</a></li>
+                    <li class="button-quote"><a href="#">Get an instant quote</a></li>
 				</ul>
 			</nav>
-			<button type="button" class="nav-button">Get an instant quote</button>
 			<span class="copyright">&copy;2017 Wondersites | All rights reserved.</span>
 		</footer>
 	</body>
