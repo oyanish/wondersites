ec6bf011cdd20fb46ecb4f940119c70bf82b2895
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-05-10 16:35:57 -0400

    adds wrap around footer elements

--- footer.php
+++ footer.php
@@ -1,19 +1,21 @@
 
 		<footer class="main-footer">
 			<?php wp_footer(); ?>
-			<div class="logo">
-				<?php get_template_part( 'img/inline', 'wondersites-logo.svg' ); ?>
-				<span class="slogan">Design | Development | Marketing</span>
-			</div>
-			<nav class="footer-menu">
-				<ul>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">About us</a></li>
-					<li><a href="#">Our work</a></li>
-					<li><a href="#">Contact us</a></li>
-                    <li class="button-quote"><a href="#">Get an instant quote</a></li>
-				</ul>
-			</nav>
+            <div class="footer-wrap">
+                <div class="logo">
+                    <?php get_template_part( 'img/inline', 'wondersites-logo.svg' ); ?>
+                    <span class="slogan">Design | Development | Marketing</span>
+                </div>
+                <nav class="footer-menu">
+                    <ul>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Our work</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li class="button-quote"><a href="#">Get an instant quote</a></li>
+                    </ul>
+                </nav>
+            </div>
 			<span class="copyright">&copy;2017 Wondersites | All rights reserved.</span>
 		</footer>
 	</body>
