0ffb0ec39c0ee75ef5463ddce4de083195f030c0
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-05-11 13:17:54 -0400

    renames theme script functions

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
-function twentyseventeen_scripts() {
+function wondersites_theme_scripts() {
     // Theme stylesheet.
-    wp_enqueue_style( 'twentyseventeen-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'wondersites-theme-style', get_stylesheet_uri() );
 }
-add_action( 'wp_enqueue_scripts', 'twentyseventeen_scripts' );
+add_action( 'wp_enqueue_scripts', 'wondersites_theme_scripts' );
 
 
