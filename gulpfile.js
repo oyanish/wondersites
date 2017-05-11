5a397b7c652f5a4dd8792c1712d14ce4876203c1
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-05-11 14:49:47 -0400

    adds autoprefixer task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('default', function() {
     livereload.listen();
@@ -12,9 +14,19 @@ gulp.task('default', function() {
 gulp.task('styles', function () {
     gulp.src('sass/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('reload', function () {
     livereload();
 });
+
+gulp.task('autoprefixer', function () {
+    gulp.src('./style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./'));
+});
+
