d8cf046493188e672a8ace7455c45c98f43cb219
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-05-22 14:22:33 -0400

    configuration for slick

--- javascript/wondersites.js
+++ javascript/wondersites.js
@@ -1,6 +1,54 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    // Toggles the mobile menu and the chevron next to the button
     $('.mobile-menu-toggle').on('click', function() {
         $('.primary-menu ul li:not(.button-quote)').slideToggle(200);
         $('.icon-chevron-thin-down').toggleClass('rotated');
+    });
+
+    // Animates and repositions form labels
+    $('.ginput_container input, .ginput_container textarea').on('focus', function() {
+        $(this).parent().siblings('.gfield_label').addClass('focused-or-filled');
+    }).on('blur', function() {
+        if ($(this).val() === '') {
+            $(this).parent().siblings('.gfield_label').removeClass('focused-or-filled');
+        }
+    });
+
+    // Initializes slick carousel
+    $('.slick-carousel').slick({
+        prevArrow: '<i class="slick-prev icon-chevron-thin-left"></i>',
+        nextArrow: '<i class="slick-next icon-chevron-thin-right"></i>',
+    });
+
+    $('.slick-gallery').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
