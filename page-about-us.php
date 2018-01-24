86fb371e38f971fc1ab8a6aafe29aa1a8e456b46
Olha Yanishevska <olhayanishevska@Olhas-iMac.local>
2018-01-24 16:06:24 -0500

    About us page

--- page-about-us.php
+++ page-about-us.php
@@ -1,45 +1,75 @@
 <?php get_header(); ?>
 
-<section class="about-us">
+<section class="about-us-banner">
 	<div class="container">
-		<h1>About Wondersites</h1>
-        <hr class="underline">
-		<?php echo get_field('about_us_text'); ?>
+		<h1><span class="blue">Our</span> story</h1>
 	</div>
 </section>
 
-<section class="team-leaders">
-	<div class="container">
-		<h2>Our team leaders</h2>
-        <hr class="underline">
-		<?php if( have_rows('team_member_information') ): ?>
-			<div class="leader-blurbs">
-				<?php while( have_rows('team_member_information') ): the_row();
-					$image = get_sub_field('member_picture');
-					$name = get_sub_field('member_name');
-					$description = get_sub_field('member_description');
-					?>
-					<div class="blurb">
-						<img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="">
-						<span><?php echo $name ?></span>
-						<?php echo $description ?>
-					</div>
-				<?php endwhile; ?>
-			</div>
-		<?php endif; ?>
-	</div>
+<section class="about-us-text">
+    <p>Wondersites was built upon the fearless drive of living outside the box. We are a small, dedicated team of visionaries who are passionate about leveraging the power of online tools to help our clients tell their unique story. We constantly thrive to live outside of our comfort zone, thus stimulating our creativity to reach its highest potential. Thirty percent of our resources are dedicated to growing our knowledge and keeping up with the latest tools and technologies to ensure that our clients stand out from the crowd. We are committed to helping our clients succeed, because your success is our success.</p>
+    <p>In 2015, Anton and Olha decided to quit their jobs and buy a 1979 Volkswagen Westfalia which they fully restored by themselves, a challenge that proved to be difficult. We did not know anything about restoring a vintage car, but with alot of dedication and perseverance we managed to restore it fully from its body and electrical wiring down to its engine and transmission. Everyone around us thought that we were crazy, but it took a visionary to see what we saw. It took us a year, but we did it and in 2016 we left on our PanAmerican coast to coast adventure that took us serveral months through three different countries. During that time, we worked on building our own business, Wondersites, in which we put the same kind of dedication and perseverance.</p>
+    <p>Thanks to our unique lifestyle, we were able to meet with clients all over Canada and Unites States. Want to have a unique experience? Meet us for a consultation in our rolling office. The coffee is on us!</p>
 </section>
 
-<section class="contact-us">
-	<div class="container">
-		<h2>Interested in working with us?</h2>
-        <hr class="underline">
-        <div class="buttons-cta">
-            <button class="big-blue">Schedule a consultation</button>
-            <span>or</span>
-            <button class="big-blue">Get in touch</button>
+<section class="team-members">
+    <div class="team-member-card">
+        <img width="150px" src="<?php bloginfo('template_url')?>/img/olha-wondersites.JPG" alt="">
+        <span class="name">Olha Yanishevska</span>
+        <span class="bio">Artisan vaporware ethical shoreditch, mumblecore activated charcoal yuccie 3 wolf moon disrupt taxidermy hexagon. Celiac bespoke man bun craft beer. Blog tote bag bicycle rights truffaut pitchfork, hexagon man braid forage mumblecore jianbing quinoa health goth meggings stumptown ennui. Organic trust fund vice, chartreuse man bun taiyaki pug cloud bread vegan tousled tumblr blue bottle. </span>
+        <div class="member-social-media">
+            <a href="#"><i class="fa fa-facebook-square"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
         </div>
-	</div>
+    </div>
+    <div class="team-member-card">
+        <img width="150px" src="<?php bloginfo('template_url')?>/img/anton-wondersites.jpg" alt="">
+        <span class="name">Anton Iancu</span>
+        <span class="bio">Artisan vaporware ethical shoreditch, mumblecore activated charcoal yuccie 3 wolf moon disrupt taxidermy hexagon. Celiac bespoke man bun craft beer. Blog tote bag bicycle rights truffaut pitchfork, hexagon man braid forage mumblecore jianbing quinoa health goth meggings stumptown ennui. Organic trust fund vice, chartreuse man bun taiyaki pug cloud bread vegan tousled tumblr blue bottle. </span>
+        <div class="member-social-media">
+            <a href="#"><i class="fa fa-facebook-square"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+    </div>
+    <div class="team-member-card">
+        <img width="150px" src="<?php bloginfo('template_url')?>/img/valentin-wondersites.jpg" alt="">
+        <span class="name">Valentin Iancu</span>
+        <span class="bio">Artisan vaporware ethical shoreditch, mumblecore activated charcoal yuccie 3 wolf moon disrupt taxidermy hexagon. Celiac bespoke man bun craft beer. Blog tote bag bicycle rights truffaut pitchfork, hexagon man braid forage mumblecore jianbing quinoa health goth meggings stumptown ennui. Organic trust fund vice, chartreuse man bun taiyaki pug cloud bread vegan tousled tumblr blue bottle. </span>
+        <div class="member-social-media">
+            <a href="#"><i class="fa fa-facebook-square"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+    </div>
+</section>
+
+<section class="wondersites-partners">
+    <div class="text-container">
+        <h2>We work with the best in the business</h2>
+        <p>At wondersites, we believe that beautiful things can be achieved through teamwork and collaboration. That is why we have a trusted network of talented partners. Whether it's for a collaboration on a project or a referral for a client, we trust in them to always deliver quality work as per the wondersites standard. Web development, copyright, design and branding, you name it, we got you covered.</p>
+    </div>
+    <div class="featured-partner">
+        <img width="150px" src="<?php bloginfo('template_url')?>/img/joey-kudish-wondersites.jpeg" alt="">
+        <div class="text">
+            <span class="title">Featured partner</span>
+            <span class="name">Joey Kudish</span>
+            <span class="company">Spark Consulting</span>
+            <span class="bio">Artisan vaporware ethical shoreditch, mumblecore activated charcoal yuccie 3 wolf moon disrupt taxidermy hexagon. Celiac bespoke man bun craft beer. Blog tote bag bicycle rights truffaut pitchfork, hexagon man braid forage mumblecore jianbing quinoa health goth meggings stumptown ennui.</span>
+        </div>
+        <div class="partner-social-media">
+            <a href="#"><i class="fa fa-facebook-square"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+    </div>
+    <h3>Are you interested in working with us?</h3>
+    <a href="<?php site_url( '/partners' ); ?>" class="become-a-partner square-blue-button">Become a partner</a>
 </section>
 
 <?php get_footer(); ?>
